19d2bbef04f35be30ae7f5a94d29fe71967b0463
hdev1004 <h.dev@kakao.com>
2024-07-10 13:59:51 +0900

    docker build

--- dockerBuild.bat
+++ dockerBuild.bat
@@ -1,2 +1,14 @@
-docker build -t hdev1004/vue-boilerplate-admin .
-docker run -d --name vue-admin -p 8085:8085 hdev1004/vue-boilerplate-admin
+set IMAGE_NAME=hdev1004/vue-boilerplate-admin
+set CONTAINER_NAME=vue-admin
+
+echo Stopping and removing any running containers named %CONTAINER_NAME%...
+docker stop %CONTAINER_NAME%
+docker rm %CONTAINER_NAME%
+
+echo Removing the image named %IMAGE_NAME%...
+docker rmi %IMAGE_NAME%
+
+echo Building the new image named %IMAGE_NAME%...
+docker build -t %IMAGE_NAME% .
+
+docker run -d --name %CONTAINER_NAME% -p 8085:8085 %IMAGE_NAME%
