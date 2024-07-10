b540213e423ac421b581747f5f2e6008269b5ede
teddy0210 <teddy.kim@tplnd.com>
2024-07-10 12:53:59 +0900

    add: 제품목록, 라우팅 수정

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const router = useRouter()
+import { warning } from '@/utils/vueAlert'
+let now = new Date()
+
+let memberTokenString = localStorage.getItem('memberToken')
+let memberToken = memberTokenString ? JSON.parse(memberTokenString) : undefined
+if (!memberToken) {
+  warning('로그인을 진행해주세요.')
+  router.push('/login')
+} else if (now.getTime() > memberToken.expires) {
+  warning('로그인이 만료되었습니다.')
+  router.push('/login')
+}
+</script>
 
 <template>
   <section>
