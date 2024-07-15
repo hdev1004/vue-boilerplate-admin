d40b07ab31d0cce36c71ca947f5b9395c0eff807
hdev1004 <h.dev@kakao.com>
2024-07-16 08:49:17 +0900

    fix: 도커 빌드 수정

--- dockerBuild.bat
+++ dockerBuild.bat
@@ -12,3 +12,6 @@ echo Building the new image named %IMAGE_NAME%...
 docker build -t %IMAGE_NAME% .
 
 docker run -d --name %CONTAINER_NAME% -p 8085:8085 %IMAGE_NAME%
+
+echo Pushing the image named %IMAGE_NAME% to Docker Hub...
+docker push %IMAGE_NAME%
