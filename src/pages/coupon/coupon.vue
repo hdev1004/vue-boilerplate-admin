7289b3ed5d7e4b92404942b55c0faa11b7d0a111
teddy0210 <teddy.kim@tplnd.com>
2024-07-24 13:42:35 +0900

    add: 쿠폰 상품 검색 추가

--- src/pages/coupon/coupon.vue
+++ src/pages/coupon/coupon.vue
@@ -1,10 +1,45 @@
 <script setup lang="ts">
-import { warning } from '@/utils/vueAlert'
+import AxiosInstance from '@/axios/axiosInstance'
+import { error, success, warning } from '@/utils/vueAlert'
 
 const inputType = ref('price')
+const couponType = ref('all')
 const imagePreview = ref<any>(null)
 const imageFile = ref(null)
+const isItemSelect = ref(false)
 const imageRef = ref<any>('')
+
+const itemList = ref<any>([])
+const searchInput = ref<String>('')
+const selectItem = ref<any>(null)
+
+//쿠폰관련
+const couponName = ref('')
+const description = ref('')
+const discount = ref(0)
+const productId = ref(null)
+
+const getItemList = async (keyword: String) => {
+  try {
+    let data = await AxiosInstance.get(
+      `/api/product-service/products/search?size=10&page=1&keyword=${keyword}`
+    )
+    //console.log('data : ', data.data.contents)
+    itemList.value = data.data.contents
+    console.log('ITEM LIST : ', itemList.value)
+  } catch (err: any) {
+    console.log(err)
+    error('오류가 발생했습니다')
+  }
+}
+
+const searchEnter = async (e: any) => {
+  if (e.key === 'Enter') {
+    getItemList(searchInput.value)
+  }
+}
+
+getItemList('')
 
 warning('쿠폰 목록은 아래에 있습니다')
 const onFileChange = (event: any) => {
@@ -17,6 +52,13 @@ const onFileChange = (event: any) => {
     }
     reader.readAsDataURL(file)
   }
+}
+
+//모달에서 아이템 셀렉트
+const itemSelect = (item: any) => {
+  success('아이템 선택 완료 : ' + item.productId)
+  selectItem.value = item
+  isItemSelect.value = false
 }
 
 const uploadClick = () => {
@@ -41,24 +83,42 @@ const uploadClick = () => {
           </div>
           <div class="input_upload">
             <input placeholder="설명을 입력하세요" />
-            <a-radio-group v-model:value="inputType" class="radio_group">
-              <a-radio-button value="price">가격</a-radio-button>
-              <a-radio-button value="percent">퍼센트</a-radio-button>
-            </a-radio-group>
-
-            <div class="input_deduct">
-              <input
-                :placeholder="
-                  inputType === 'price' ? '공제될 가격을 입력하세요' : '공제될 퍼센트를 입력하세요'
-                "
-                type="number"
-                :max="100"
-              />
-              <div>{{ inputType === 'price' ? '₩' : '%' }}</div>
+            <div class="input_sales">
+              <a-radio-group v-model:value="inputType" class="radio_group" size="large">
+                <a-radio-button value="price">가격</a-radio-button>
+                <a-radio-button value="percent">퍼센트</a-radio-button>
+              </a-radio-group>
+              <div class="input_deduct">
+                <input
+                  :placeholder="
+                    inputType === 'price'
+                      ? '공제될 가격을 입력하세요'
+                      : '공제될 퍼센트를 입력하세요'
+                  "
+                  type="number"
+                  :max="100"
+                />
+                <div>{{ inputType === 'price' ? '₩' : '%' }}</div>
+              </div>
             </div>
-            <div class="upload_btn">등록</div>
+
+            <div class="item_select_container">
+              <a-radio-group v-model:value="couponType" class="coupon_size_group" size="large">
+                <a-radio-button value="all">쿠폰 전체 적용</a-radio-button>
+                <a-radio-button value="item">특정 상품 적용</a-radio-button>
+              </a-radio-group>
+
+              <div v-if="couponType === 'item'">
+                <div class="item_select_btn" @click="isItemSelect = true">상품 선택</div>
+              </div>
+            </div>
+
+            <div v-if="selectItem && couponType === 'item'" class="item_select_name">
+              <div>선택된 상품 : {{ selectItem.name }}</div>
+            </div>
           </div>
         </div>
+        <div class="upload_btn">등록</div>
       </div>
       <div class="coupon_right">
         <section class="title_container">
@@ -73,7 +133,11 @@ const uploadClick = () => {
             <div>수정</div>
             <div>삭제</div>
           </div>
-          <div class="coupon_contents" v-for="item in [1, 2, 3, 4, 5]" v-bind:key="`item${item}`">
+          <div
+            class="coupon_contents"
+            v-for="(item, index) in itemList"
+            v-bind:key="`item${index}`"
+          >
             <div class="coupon_image">
               <img src="@/assets/images/coupon2.png" />
             </div>
@@ -87,6 +151,43 @@ const uploadClick = () => {
             </div>
           </div>
         </section>
+      </div>
+    </div>
+
+    <div :class="`coupon_item_select_container coupon_item_select_container_${isItemSelect}`">
+      <div class="coupon_item_select_background" @click="isItemSelect = false"></div>
+      <div class="coupon_item_select_modal">
+        <div class="coupon_item_select_row">
+          <div class="coupon_item_select_close">
+            <img src="@/assets/images/close.png" @click="isItemSelect = false" />
+          </div>
+          <div class="coupon_item_select_title">상품 선택</div>
+          <div class="coupon_item_select_search">
+            <input placeholder="검색" v-model="searchInput" @keypress="searchEnter" />
+          </div>
+        </div>
+        <div class="coupon_item_select_items">
+          <div class="coupon_item_select_header">
+            <div>사진</div>
+            <div class="coupon_item_select_item_name">상품명</div>
+            <div>선택</div>
+          </div>
+
+          <div
+            class="coupon_item_select_item"
+            v-for="(item, index) in itemList"
+            v-bind:key="`itemlist${index}`"
+          >
+            {{ console.log('ITEM CARD : ', item) }}
+            <div class="coupon_item_select_thumbnail">
+              <img :src="`/api/product-service/products/images/${item.thumbnailImageId}`" />
+            </div>
+            <div class="coupon_item_select_item_name">{{ item.name }}</div>
+            <div class="coupon_item_check">
+              <img src="@/assets/images/checkmark.png" @click="itemSelect(item)" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
