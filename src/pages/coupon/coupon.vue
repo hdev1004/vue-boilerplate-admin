74a03502bb6ac0a8a5960651caa2156b54019d86
teddy0210 <teddy.kim@tplnd.com>
2024-07-25 11:49:16 +0900

    fix: 쿠폰 페이지 수정

--- src/pages/coupon/coupon.vue
+++ src/pages/coupon/coupon.vue
@@ -12,6 +12,7 @@ const imageRef = ref<any>('')
 const itemList = ref<any>([])
 const searchInput = ref<String>('')
 const selectItem = ref<any>(null)
+const couponList = ref<any>([])
 
 //쿠폰관련
 const couponName = ref('')
@@ -24,12 +25,27 @@ const getItemList = async (keyword: String) => {
     let data = await AxiosInstance.get(
       `/api/product-service/products/search?size=10&page=1&keyword=${keyword}`
     )
+    if (data === null) return
+
     //console.log('data : ', data.data.contents)
     itemList.value = data.data.contents
     console.log('ITEM LIST : ', itemList.value)
   } catch (err: any) {
     console.log(err)
     error('오류가 발생했습니다')
+  }
+}
+
+const getCouponList = async () => {
+  try {
+    let data = await AxiosInstance.get('/api/order-service/coupon')
+    if (data === null) return
+    couponList.value = data.data.coupons
+
+    console.log('couponList : ', couponList.value)
+  } catch (err: any) {
+    console.log(err)
+    error('오류가 발생했습니다.')
   }
 }
 
@@ -40,12 +56,14 @@ const searchEnter = async (e: any) => {
 }
 
 getItemList('')
+getCouponList()
 
 warning('쿠폰 목록은 아래에 있습니다')
 const onFileChange = (event: any) => {
   const file = event.target.files[0]
   if (file) {
     imageFile.value = file
+    console.log('FIle : ', imageFile.value)
     const reader = new FileReader()
     reader.onload = (e: any) => {
       imagePreview.value = e.target.result
@@ -63,6 +81,59 @@ const itemSelect = (item: any) => {
 
 const uploadClick = () => {
   imageRef.value.click()
+}
+
+const addCoupon = async () => {
+  let axiosConfig = {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
+  }
+  try {
+    if (imageFile.value === null) {
+      error('이미지는 필수 항목입니다.')
+      return
+    }
+    if (couponName.value.trim() === '') {
+      error('쿠폰 제목을 입력해주세요.')
+      return
+    }
+    if (discount.value <= 0) {
+      error('할인율을 0이상 입력해주세요.')
+      return
+    }
+    if (inputType.value === 'item' && productId.value === null) {
+      error('특정 상품을 선택해주세요.')
+      return
+    }
+    console.log('IMAGE : ', imageFile.value)
+
+    let param: any = {
+      multipartFile: imageFile.value,
+      name: couponName.value,
+      description: '',
+      discount: discount.value
+    }
+
+    if (inputType.value === 'item') {
+      param['productId'] = productId.value
+    }
+
+    let data = await AxiosInstance.post('/api/order-service/coupon', param, axiosConfig)
+    if (data === null) return
+    success('쿠폰이 등록되었습니다.')
+
+    imagePreview.value = null
+    imageFile.value = null
+    couponName.value = ''
+    discount.value = 0
+    productId.value = null
+    selectItem.value = null
+    inputType.value = 'all'
+  } catch (err: any) {
+    console.log(err)
+    error('쿠폰 등록 중 오류가 발생했습니다.')
+  }
 }
 </script>
 
@@ -82,14 +153,16 @@ const uploadClick = () => {
             <img v-else src="@/assets/images/addImage.png" />
           </div>
           <div class="input_upload">
-            <input placeholder="설명을 입력하세요" />
+            <input placeholder="제목을 입력하세요" v-model="couponName" />
             <div class="input_sales">
               <a-radio-group v-model:value="inputType" class="radio_group" size="large">
                 <a-radio-button value="price">가격</a-radio-button>
                 <a-radio-button value="percent">퍼센트</a-radio-button>
               </a-radio-group>
+
               <div class="input_deduct">
                 <input
+                  v-model="discount"
                   :placeholder="
                     inputType === 'price'
                       ? '공제될 가격을 입력하세요'
@@ -118,7 +191,7 @@ const uploadClick = () => {
             </div>
           </div>
         </div>
-        <div class="upload_btn">등록</div>
+        <div class="upload_btn" @click="addCoupon">등록</div>
       </div>
       <div class="coupon_right">
         <section class="title_container">
@@ -130,22 +203,18 @@ const uploadClick = () => {
             <div class="coupon_image">사진</div>
             <div class="coupon_description">쿠폰 설명</div>
             <div>공제 금액</div>
-            <div>수정</div>
             <div>삭제</div>
           </div>
           <div
             class="coupon_contents"
-            v-for="(item, index) in itemList"
+            v-for="(item, index) in couponList"
             v-bind:key="`item${index}`"
           >
             <div class="coupon_image">
-              <img src="@/assets/images/coupon2.png" />
+              <img :src="`/api/order-service/coupon/images/${item.couponImage.couponImageId}`" />
             </div>
-            <div class="coupon_description">쿠폰 설명</div>
-            <div>10%</div>
-            <div class="coupon_edit">
-              <img src="@/assets/images/edit.png" />
-            </div>
+            <div class="coupon_description">{{ item.name }}</div>
+            <div>{{ discount.toLocaleString() }}</div>
             <div class="coupon_delete">
               <img src="@/assets/images/cancel.png" />
             </div>
@@ -178,7 +247,6 @@ const uploadClick = () => {
             v-for="(item, index) in itemList"
             v-bind:key="`itemlist${index}`"
           >
-            {{ console.log('ITEM CARD : ', item) }}
             <div class="coupon_item_select_thumbnail">
               <img :src="`/api/product-service/products/images/${item.thumbnailImageId}`" />
             </div>
